c331d123040d30a0d6e519975be90fe08c7aadb1
Caique de Castro <castro.caique@gmail.com>
2022-12-09 15:20:32 -0300

    feat: Validate zipcode

--- src/script.js
+++ src/script.js
@@ -8,13 +8,31 @@ if ("serviceWorker" in navigator) {
         });
 }
 
+const ZIPCODE_REGEX = /[0-9]{5}\-?[0-9]{3}/;
+
 const zipcodeForm = document.querySelector("#zipcode-form");
+const zipcodeInput = document.querySelector("#zipcode");
 const zipcodeStreet = document.querySelector("#zipcode-street");
 const zipcodeNeighborhood = document.querySelector("#zipcode-neighborhood");
 const zipcodeCity = document.querySelector("#zipcode-city");
 const zipcodeState = document.querySelector("#zipcode-state");
 const searchMessage = document.querySelector("#search-message");
 const searchResult = document.querySelector("#search-result");
+
+const validateZipcode = (zipcode) => {
+    const cleanZipcode = zipcode.trim();
+    zipcodeInput.value = cleanZipcode;
+
+    if (!cleanZipcode.length) {
+        throw new Error("Fill zipcode input");
+    }
+
+    if (!ZIPCODE_REGEX.test(cleanZipcode)) {
+        throw new Error("Fill valid zipcode");
+    }
+
+    return cleanZipcode.replace("-", "");
+};
 
 const fillAddress = (data) => {
     zipcodeStreet.textContent = data.street;
@@ -30,16 +48,13 @@ zipcodeForm.addEventListener("submit", async (event) => {
     const zipcode = formData.get("zipcode");
     searchResult.style.display = "none";
 
-    if (!zipcode.length) {
-        searchMessage.textContent = "Fill zipcode input...";
-        return;
-    }
-
     try {
+        const validZipcode = validateZipcode(zipcode);
+
         searchMessage.textContent = "Loading...";
         fillAddress({});
 
-        const response = await fetch(`https://brasilapi.com.br/api/cep/v2/${zipcode}`);
+        const response = await fetch(`https://brasilapi.com.br/api/cep/v2/${validZipcode}`);
 
         const responseData = await response.json();
 
